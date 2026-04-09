b503b817f1acafcd299ec32bde37b44479363647
mertkrglu <mertkrgluphoto@gmail.com>
2026-04-09 18:16:00 +0300

    düzenleme

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-odev-9/',
     define: {
       global: 'window',
     },
